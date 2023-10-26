d563819ca0e38c290c21c55ce8d0183566d413d0
Tanishka Agarwal <agarwaltani642004@gmail.com>
2023-10-26 23:52:08 +0530

    fixing slidebar

--- script.js
+++ script.js
@@ -5,75 +5,160 @@ let artist = document.querySelector(".artist");
 let pause = document.querySelector(".pause");
 let previous = document.querySelector(".previous");
 let next = document.querySelector(".next");
-let mainSlider = document.getElementById("#main-slider");
-let volume = document.querySelector(".volume");
+let mainSlider = document.getElementById("main-slider");
+let Volume = document.querySelector(".volume-slider");
 let time = document.querySelector(".time");
 let duration = document.querySelector(".duration");
-
-let musicNumber =0;
-let IsPlaying =false;
-let curr_track = document.createElement('audio');
+var updateTimer;
+let track_index = 0;
+let IsPlaying = false;
+let curr_track = document.createElement("audio");
 
 let musicLibrary = [
-    {
-        name:"Kesariya",
-        artist:"Arijit Singh",
-        image:,
-        path:
-    },
-    {
-        name:"People You Know X Starboy",
-        artist:"Unknown",
-        image:,
-        path:
-    }
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    image: "bannerimages/kesariya.jpg",
+    path: "audiofiles/Kesariya.mp3",
+  },
+  {
+    name: "People You Know X Starboy",
+    artist: "Unknown",
+    image: "bannerimages/pykxstar.jpg",
+    path: "audiofiles/PeopleYouKnowxStarboy.mp3",
+  },
+  {
+    name: "Cold",
+    artist: "Neffex",
+    image: "bannerimages/cold.jpg",
+    path: "audiofiles/NeffexCold.mp3",
+  },
+  {
+    name: "Perfect",
+    artist: "One-Direction",
+    image: "bannerimages/perfect.png",
+    path: "audiofiles/Perfect.mp3",
+  },
+  {
+    name: "Style",
+    artist: "Taylor Swift",
+    image: "bannerimages/style.webp",
+    path: "audiofiles/Style.mp3",
+  },
+  {
+    name: "The Nights",
+    artist: "Avicii",
+    image: "bannerimages/nights.jpg",
+    path: "audiofiles/TheNights.mp3",
+  },
+  {
+    name: "Treat You Better",
+    artist: "Shawn Mendes",
+    image: "bannerimages/tyb.jpg",
+    path: "audiofiles/TreatYouBetter.mp3",
+  },
+  {
+    name: "Tourner Dans Le Vide",
+    artist: "Indilla",
+    image: "bannerimages/ind.jpg",
+    path: "audiofiles/TournerDansLeVide.mp3",
+  },
 ];
 
-function loadTrack(track_index){
-    clearInterval(updateTime);
-    resetValues();
-    curr_track.src = musicLibrary[track_index].path;
-    curr_track.load();
-    banner.style.backgroundImage = "url("+ musicLibrary[track_index].image +")";
-    name.textContent = musicLibrary[track_index].name;
-    artist.textContent = musicLibrary[track_index].artist;
-    nowPlaying.textContent = "PLAYING" + musicLibrary[track_index].name +"BY"+musicLibrary[track_index].artist;
+function resetValue() {
+  time.textContent = "00:00";
+  duration.textContent = "00:00";
+  mainSlider.value = 0;
+}
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
 
-    let updateTimer =setInterval(mainSlider,3000);
-    curr_track.addEventListener("ended",nextTrack);
-    
+  resetValue();
+  curr_track.src = musicLibrary[track_index].path;
+  curr_track.load();
+  banner.style.backgroundImage = "url(" + musicLibrary[track_index].image + ")";
+  name.textContent = musicLibrary[track_index].name;
+  artist.textContent = musicLibrary[track_index].artist;
+  nowPlaying.textContent =
+    "Playing " +
+    musicLibrary[track_index].name +
+    " by " +
+    musicLibrary[track_index].artist;
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
 }
-function resetValue(){
-    time.textContent= "00:00";
-    duration.textContent="00:00";
-    mainSlider.value=0;
-}
-function pauseplay(){
-    if(IsPlaying) play();
-    else pause();
-}
-function play(){
-    curr_track.play();
-    IsPlaying= true;
-    pause.innerHTML =<i class="fa-solid fa-pause" ></i>;
 
+function playTrack() {
+  curr_track.play();
+  console.log(curr_track);
+
+  IsPlaying = true;
+  pause.innerHTML = `<i class="fa-solid fa-pause" ></i>`;
 }
-function pause(){
-    curr_track.pause();
-    IsPlaying=false;
-    pause.innerHTML=<i class="fa-solid fa-play"></i>;
+function pauseTrack() {
+  curr_track.pause();
+  IsPlaying = false;
+  pause.innerHTML = `<i class="fa-solid fa-play"></i>`;
 }
-function nextTrack(){
-    if(track_index < musicLibrary.length -1)
-    track_index += 1;
-    else track_index=0;
-    loadTrack(track_index);
-    play()
+function pauseplay() {
+  if (!IsPlaying) {
+    playTrack();
+  } else {
+    pauseTrack();
+  }
 }
-function previous(){
-    if(track_index > 0)
-    track_index-=1;
-    else track_index=musicLibrary.length -1;
-loadTrack(track_index);
-play();
+function nextTrack() {
+  if (track_index < musicLibrary.length - 1) track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  pauseplay();
 }
+function previousTrack() {
+  if (track_index > 0) track_index -= 1;
+  else track_index = musicLibrary.length - 1;
+  loadTrack(track_index);
+  pauseplay();
+}
+function seek() {
+  seek = curr_track.duration * (mainSlider.value / 100);
+  curr_track.currentTime = seek;
+  console.log("Seeeeeeeeeeek");
+}
+function seekUpdate() {
+  let seekPosition = 0;
+  if(!isNaN(curr_track.duration)){
+ seekPosition = curr_track.currentTime*(100 / curr_track.duration);
+ mainSlider.value=seekPosition;
+  let currentMinutes = Math.floor(curr_track.currentTime / 60);
+  let currentSecond = Math.floor(curr_track.currentTime - currentMinutes * 60);
+  let durationminute = Math.floor(curr_track.duration / 60);
+  let durationseconds = Math.floor(curr_track.duration - durationminute * 60);
+
+//   console.log(currentMinutes);
+  if (currentMinutes < 10) {
+    currentMinutes = "0" + currentMinutes;
+  }
+  if (currentSecond < 10) {
+    currentSecond = "0" + currentSecond;
+  }
+  if (durationminute < 10) {
+    durationminute = "0" + durationminute;
+  }
+  if (durationseconds < 10) {
+    durationseconds = "0" + durationseconds;
+  }
+  time.textContent = currentMinutes + ":" + currentSecond;
+  duration.textContent = durationminute + ":" + durationseconds;
+}}
+
+function SetVolume() {
+  curr_track.volume = Volume.value/100;
+  
+}
+
+loadTrack(0);
+pause.addEventListener("click", pauseplay);
+next.addEventListener("click", nextTrack);
+previous.addEventListener("click", previousTrack);
+mainSlider.addEventListener("input",seek);
+Volume.addEventListener("input",SetVolume);
